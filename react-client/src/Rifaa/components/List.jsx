f83f3f18c1c6ce854da061dbb9261eb292d45f24
MohammadAlRifai <mohammad94sar@gmail.com>
2018-04-23 22:13:21 +0300

    Add info

--- react-client/src/Rifaa/components/List.jsx
+++ react-client/src/Rifaa/components/List.jsx
@@ -4,9 +4,9 @@ import ListItem from './ListItem.jsx';
 const List = (props) => (
   <div>
   <h1> this is my disease </h1> 
-    <h4> List painent </h4>
-    There are { props.painents.length } painents.
-    { props.painents.map(painent => <ListItem painent={painent}/>)}
+    <h4> List patient </h4>
+    There are { props.patients.length } patients.
+    { props.patients.map(patient => <ListItem patient={patient}/>)}
   </div>
 )
 
