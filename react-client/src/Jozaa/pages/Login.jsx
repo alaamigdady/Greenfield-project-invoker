a851b11c51777f0a829fa3ab44b85786ce146f94
MohammadJozaa7 <36266902+MohammadJozaa7@users.noreply.github.com>
2018-04-23 19:32:12 +0300

    “T”

--- react-client/src/Jozaa/pages/Login.jsx
+++ react-client/src/Jozaa/pages/Login.jsx
@@ -28,7 +28,7 @@ const password={
   fontWeight:'bold',
   textAlign:'center',
 };
-//the style for the button Sign In
+//the style for the button Login
 const button={
   padding:'5px',
   //this three to make it center
@@ -53,7 +53,50 @@ const button2={
   marginTop:'10px'
 };
 //the page login what inside render
-class SignIn extends React.Component {
+class Login extends React.Component {
+  //constructor to undestand state
+  constructor(){
+    super();
+    //all the data save before sent in state
+    this.state={
+      userName: '',
+      password: '',
+    };
+  }
+  //when change  ... change the
+  //username
+  onWrite3 (e) {
+    this.setState({
+      userName: e.target.value,
+    });
+  };
+  //password
+  onWrite4 (e) {
+    this.setState({
+      password: e.target.value,
+    });
+  };
+  //for sign in button
+  login() {
+    console.log(`YOU WANT LOGIN : ${this.state.userName} ${this.state.password} `);
+    //ajax request
+    $.ajax({
+      type: 'POST',
+      url: '/login',
+      data: {
+        userName: `${this.state.userName}`,
+        password: `${this.state.password}`
+      },
+      //when success do this
+      success: function () {
+        console.log('SUCCESS LOGIN');
+      },
+      //when error do this
+      error: function (){
+        console.log('FAILED LOGIN')
+      },
+    }); 
+  }
   //what render -----------------need change
   render () { 
     return (
@@ -61,20 +104,21 @@ class SignIn extends React.Component {
         <h2 style={header}>Welcome To Medical Record</h2>
         
         <h3 style={user}>Username:</h3>
-        <input placeholder="Username" style={input} ></input>
+        <input value={this.state.userName} onChange={this.onWrite3.bind(this)} placeholder="Username" style={input} ></input>
      
         <h3 style={password}>Password:</h3>
-        <input placeholder="Password" style={input} ></input>
+        <input value={this.state.password} onChange={this.onWrite4.bind(this)} placeholder="Password" style={input} ></input>
        
-        <button style={button}>Sign In</button>
+        <button onClick={this.login.bind(this)} style={button}>Login</button>
         <h5 style={password}>Dont have an account ?</h5>
+        {/*  sign up go to router   */}
         <button style={button2}>Sign Up</button>
       </div>
     )
   }
 }
 //export this component to can use
-export default SignIn;
+export default Login;
 
 
 
