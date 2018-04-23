5c05668333613137bdd3dd274491bb8be98dfe27
Belalfaouri <belalfaourig@gmail.com>
2018-04-23 10:17:00 +0300

    login and sign up functions

--- server/routes.js
+++ server/routes.js
@@ -1,33 +1,77 @@
 var router=require('express').Router();
 var controller=require('../database-mongo/PatientController');
+var utils=require('./utils')
+var bcrypt=require('bcrypt')
+var session=require('express-session');
+
+
 //routes and handling requests.
 
-router.route('/').get(function(req,res){
+router.get('/',utils.checkUser,function(req,res){
   res.send('hi')
 })
 
-router.route('/login').get()
+router.route('/login').get(function(req,res){
+  res.send('hi')
+})
 
-router.route('/login').post()
+router.route('/login').post(function(req,res){
+  var username=req.body.username;
+  var password=req.body.password;
+
+  User.find({username:username},function(err,user){
+    if(!user){
+      return res.redirect('/login')
+    }
+    bcrypt.compare(password,user.password,function(err,match){
+      if(match){
+        utils.createSession(req,res,user)
+      }else{
+        res.redirect('/login')
+
+      }
+    })
+  })
+})
 
 router.route('/signup').get()
 
-router.route('/signup').post()
+router.route('/signup').post(function(req,res){
+  var username=req.body.username;
+  var password=req.body.password;
+
+  User.find({username:username},function(err,user){
+    if(!user){
+      bcrypt.hash(password,null,function(err,hash){
+
+          var user=new User({username:username,password:hash})
+          user.save(function(err,user){
+
+            utils.createSession(req,res,user)
+          })
+
+          })
+  }else{
+    console.log("User already exists!");
+    res.redirect('/signup');
+  }
+  })
+})
 
 //retrieve a pateint.
-router.route('/patient').get(controller.retrieveOne)
+router.route('/patient').get(utils.checkUser,controller.retrieveOne)
 
 //create a patient.
-router.route('/patient').post(controller.createOne)
+router.route('/patient').post(utils.checkUser,controller.createOne)
 
 //update patient information.
-router.route('/patient').put(controller.updateOne)
+router.route('/patient').put(utils.checkUser,controller.updateOne)
 
 //delete a patient.
-router.route('/patient').delete(controller.delete)
+router.route('/patient').delete(utils.checkUser,controller.delete)
 
 //get all patients
-router.route('/patients').get(controller.retrieveAll)
+router.route('/patients').get(utils.checkUser,controller.retrieveAll)
 
 
 
