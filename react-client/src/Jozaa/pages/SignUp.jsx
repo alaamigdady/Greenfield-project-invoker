c3bbe2827815e78657116a74bae983f99e9008e9
MohammadJozaa7 <36266902+MohammadJozaa7@users.noreply.github.com>
2018-04-24 13:21:12 +0300

    style home page

--- react-client/src/Jozaa/pages/SignUp.jsx
+++ react-client/src/Jozaa/pages/SignUp.jsx
@@ -2,23 +2,44 @@
 import React from 'react';
 //to work in ajax
 import $ from 'jquery';
-//the style for the two main header
+//the style for the main header
 const header={
   color:'black',
   fontWeight:'bold',
-  textAlign:'center'
+  textAlign:'center',
+  fontSize:'20px',
 };
-//the style for the first and last name
-const name={
+//the style for first name
+const fName={
   color:'#9900cc',
   fontWeight:'bold',
   textAlign:'center',
+  fontSize:'10px',
+  marginBottom:'-10px',
+};
+//the style for last name
+const lName={
+  color:'#9900cc',
+  fontWeight:'bold',
+  textAlign:'center',
+  fontSize:'10px',
+  marginBottom:'-10px',
 };
 //the style for the user
 const user={
-  color:'blue',
+  color:'#294866',
   fontWeight:'bold',
   textAlign:'center',
+  fontSize:'10px',
+  marginBottom:'-10px',
+};
+//the style for the password
+const password={
+  color:'#773060',
+  fontWeight:'bold',
+  textAlign:'center',
+  fontSize:'10px',
+  marginBottom:'-10px',
 };
 //the style for the user and password input
 const input={
@@ -27,12 +48,8 @@ const input={
   display: 'block',
   marginRight: 'auto',
   marginLeft: 'auto',
-};
-//the style for the password
-const password={
-  color:'red',
-  fontWeight:'bold',
-  textAlign:'center'
+  color:'black',
+  fontSize:'8px',
 };
 //the style for the button Sign Up
 const button={
@@ -41,23 +58,13 @@ const button={
   display: 'block',
   marginRight: 'auto',
   marginLeft: 'auto',
-  backgroundColor: 'green',
+  backgroundColor: '#773060',
   color: 'white',
-  border: '2px solid',
-  marginTop:'10px'
+  border: '2px solid #773060',
+  marginTop:'20px',
+  fontSize:'10px',
 };
-//the style for the button Login
-const button2={
-  padding:'5px',
-  //this three to make it center
-  display: 'block',
-  marginRight: 'auto',
-  marginLeft: 'auto',
-  backgroundColor: 'black',
-  color: 'white',
-  border: '2px solid',
-  marginTop:'10px'
-};
+
 //the page login what inside render
 class SignUp extends React.Component {
   //constructor to undestand state
@@ -118,24 +125,8 @@ class SignUp extends React.Component {
         console.log('FAILED SIGN UP')
       },
     }); 
-    this.props.history.push(path);
+    //this.props.history.push(path);
 
-  };
-  //when press login go to login page
-  login() {
-    //ajax request for login
-    $.ajax({
-      type: 'GET',
-      url: '/login',
-      //when success do this
-      success: function (res) {
-        console.log('SUCCESS GO TO LOGIN',typeof res);
-      },
-      //when error do this
-      error: function (){
-        console.log('FAILED GO TO LOGIN')
-      },
-    }); 
   };
   //what render -----------------need change style to be nice
   render () {
@@ -143,17 +134,25 @@ class SignUp extends React.Component {
       <div>
         <h2 style={header}>Welcome To Medical Record</h2>
 
-        <h3 style={name}>First name:</h3>
-        <input value={this.state.firstName} onChange={this.onWrite1.bind(this)} placeholder="Insert your first name" style={input} ></input>
+        <h3 style={fName}>First name: 
+          <input value={this.state.firstName} onChange={this.onWrite1.bind(this)} placeholder="First name" style={input}>
+          </input>
+        </h3>
+        
+        <h3 style={lName}>Last name: 
+          <input value={this.state.lastName} onChange={this.onWrite2.bind(this)} placeholder="Last name" style={input}>
+          </input>
+        </h3>
+        
+        <h3 style={user}>Username:
+         <input  value={this.state.userName} onChange={this.onWrite3.bind(this)} placeholder="Username" style={input}>
+         </input>
+        </h3>
 
-        <h3 style={name}>Last name:</h3>
-        <input value={this.state.lastName} onChange={this.onWrite2.bind(this)} placeholder="Insert your last name" style={input} ></input>
-
-        <h3 style={user}>Username:</h3>
-        <input  value={this.state.userName} onChange={this.onWrite3.bind(this)} placeholder="Insert your username" style={input} ></input>
-
-        <h3 style={password}>Password:</h3>
-        <input value={this.state.password} onChange={this.onWrite4.bind(this)} placeholder="Insert your password" style={input} ></input>
+        <h3 style={password}>Password:
+         <input type='password' value={this.state.password} onChange={this.onWrite4.bind(this)} placeholder="Password" style={input}>
+         </input>
+        </h3>
 
         <button onClick={this.saveUser.bind(this)} style={button}>Sign Up</button>
 
