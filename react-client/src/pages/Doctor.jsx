8a2c3239488da3d65c06047e8cd488d1c4e7d5ea
hmtirawi <hmtirawi@gmail.com>
2018-05-03 08:18:52 +0300

    handling name

--- react-client/src/pages/Doctor.jsx
+++ react-client/src/pages/Doctor.jsx
@@ -2,6 +2,36 @@ import React from 'react';
 //to work in ajax
 import $ from 'jquery';
 
+
+const body={
+	backgroundColor: '#BDF1F6',
+	fontFamily: 'sans-serif',
+	fontSize: '0.85em',
+	color: 'rgba(66,66,66 ,1)',
+	display: 'flex',
+	justifyContent: 'center',
+	alignItems: 'center',
+	height: '100vh',
+	width: '100vw',
+}
+
+
+
+const button={
+	borderRadius: '40px',
+	backgroundColor: '#d22d10',
+	border: 'none',
+	color: '#FFFFFF',
+	textAlign: 'center',
+	fontSize: '28px',
+	padding: '20px',
+	width: '200px',
+	transition: 'a1ll 0.5s',
+	cursor: 'pointer',
+	margin: '5px',
+}
+
+
 class Doctor extends React.Component {
 	constructor(){
 		super();
@@ -11,19 +41,19 @@ class Doctor extends React.Component {
 	}
 
 	componentDidMount() {
-    console.log('hi',this.state.info);
-    const that=this
-    $.ajax({
-      type: 'GET',
-      url: '/getInfo',
-      success: function (res) {
+		console.log('hi',this.state.info);
+		const that=this
+		$.ajax({
+			type: 'GET',
+			url: '/getInfo',
+			success: function (res) {
       	//console.log('hh',res)
       	that.setState({info:res })
       	//console.log(that.state.info)
       	
       }
   })
-}
+	}
 
 
 	add (){
@@ -39,43 +69,51 @@ class Doctor extends React.Component {
 	}
 
 	logout(){
-    console.log('you try to logoutDR');
-    const that=this
+		console.log('you try to logoutDR');
+		const that=this
     //ajax request to logout
     $.ajax({
-      type: 'GET',
-      url: '/logout',
+    	type: 'GET',
+    	url: '/logout',
       //when success do this
       success: function (res) {
-        alert(res);
-        that.setState({loggedIn:false});
-        window.location.href= window.location.origin+'/login'
+      	alert(res);
+      	that.setState({loggedIn:false});
+      	window.location.href= window.location.origin+'/login'
       },
       //when error do this
       error: function (){
-        alert('Failed logout please try again DR');
-        console.log('Failed logout please try again DR');
+      	alert('Failed logout please try again DR');
+      	console.log('Failed logout please try again DR');
       },
-    }) 
-  }
+  }) 
+}
 
-	render() {
-		return (
-			<div>
-			<button onClick={this.add.bind(this)}>Add Record</button>
-			<button onClick={this.search.bind(this)}>Search</button>
-			<button onClick={this.logout.bind(this)}>Log Out</button>
-			<button onClick={this.update.bind(this)}>Update your Profile</button>
-			<div>
-				{this.state.info.map((obj)=>
-					<h3> name:{obj.firstName + " " +obj.lastName } , age:{obj.age } </h3>)
-				}
-
-			</div>
-
-			</div>
-			)
+render() {
+	return (
+		<div style={body}>
+		<div>
+		<button style={button} onClick={this.add.bind(this)}><span>Add Record </span></button>
+		</div>
+		<br></br>
+		<br></br>
+		<button style={button} onClick={this.search.bind(this)}>Search</button>
+		<br></br>
+		<br></br>
+		<button style={button} onClick={this.logout.bind(this)}>Log Out</button>
+		<br></br>
+		<br></br>
+		<button style={button} onClick={this.update.bind(this)}>Update your Profile</button>
+		<div>
+		{this.state.info.map((obj)=>
+			<h3> name:{obj.firstName + " " +obj.lastName } , age:{obj.age } </h3>)
 	}
+
+	</div>
+
+	</div>
+	)
+}
 
 }
 
